7ef361b4c95918c937e0ee785fa3af79b98e2fe4
Nurbolot <154671887+nyrb0@users.noreply.github.com>
2024-07-09 16:43:46 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+onst nextConfig = {
+  //   images, когда изб. использовано строннего истосника надо указать обязательно
+  //   poweredByHeader чтобы не узнали на чем написан сайт
+  //   experimental: {
+  //     optimizePackageImports: ["названия модуля","названия модуля","названия модуля"],
+  //   },
+  //   webpack: {}, для webpack
+  images: {
+    domains: ["localhost"],
+  },
 
 module.exports = nextConfig
